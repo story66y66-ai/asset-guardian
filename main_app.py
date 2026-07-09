aafa0b7963dec305bd9ca7e7f6e60709ad780eea
story66y66-ai <story66y66@gmail.com>
2026-07-09 11:46:47 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,1 +1,9 @@
 
+import streamlit as st
+
+st.set_page_config(page_title="澄玄大學", layout="wide", page_icon="🎓")
+st.title("🎓 歡迎來到《澄玄大學》")
+
+st.write("---")
+if st.button("🏫 進入 語言學院 (點擊進入單字農場)", use_container_width=True):
+    st.switch_page("english_class.py")
