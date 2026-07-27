973130f6f1417c6721eefeb4038e58df383ed91f
story66y66-ai <story66y66@gmail.com>
2026-07-27 17:18:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,11 @@ st.title("🎓 歡迎來到《澄玄大學》")
 st.write("---")
 st.subheader("📚 語言學院")
 
-# 關鍵：將跳轉指令放在 button 被點擊的事件內，不要放在最外層
 if st.button("進入語言學院"):
     st.switch_page("pages/english_class.py")
 
 st.write("---")
 st.subheader("🍲 食品學院")
 
-if st.button("進入食品學院"):
-    st.switch_page("pages/food_class.py")
+if st.button("進入烘焙教室"):
+    st.switch_page("pages/baking_class.py")
