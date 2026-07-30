d69322203a8df2dd8caa029a935d4f4bd284a368
story66y66-ai <story66y66@gmail.com>
2026-07-30 14:16:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,12 @@ st.title("🎓 歡迎來到《澄玄大學》")
 st.write("---")
 st.subheader("📚 語言學院")
 
-# 使用兩欄並排，放英文學院與中文學院
-col1, col2 = st.columns(2)
+# 上下垂直排列，手機瀏覽也完美適配
+if st.button("進入英文學院"):
+    st.switch_page("pages/english_class.py")
 
-with col1:
-    if st.button("進入英文學院"):
-        st.switch_page("pages/english_class.py")
-
-with col2:
-    if st.button("進入中文學院"):
-        st.switch_page("pages/chinese_class.py")
+if st.button("進入中文學院"):
+    st.switch_page("pages/chinese_class.py")
 
 st.write("---")
 st.subheader("🍲 食品學院")
