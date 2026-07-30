440d0161a1ffa58594b2b188b2648424d0e163a0
story66y66-ai <story66y66@gmail.com>
2026-07-30 15:37:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,15 @@ st.title("🎓 歡迎來到《澄玄大學》")
 st.write("---")
 st.subheader("📚 語言學院")
 
-# 上下垂直排列，手機瀏覽也完美適配
 if st.button("進入英文學院"):
     st.switch_page("pages/english_class.py")
 
 if st.button("進入中文學院"):
     st.switch_page("pages/chinese_class.py")
+
+# 新增進入萬句旗艦成語挑戰賽的按鈕
+if st.button("進入萬句旗艦成語挑戰賽"):
+    st.switch_page("idiom_game.py")
 
 st.write("---")
 st.subheader("🍲 食品學院")
