19243475b577cc075a2e01225534a2cff2444b9b
story66y66-ai <story66y66@gmail.com>
2026-08-01 17:39:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,19 @@ st.write("---")
 st.subheader("💻 電腦學院")
 
 if st.button("進入電腦學院"):
-    st.switch_page("pages/1_computer_class.py")
+    st.switch_page("pages/1_computer_class_電腦學院.py")
 
 st.write("---")
 st.subheader("📚 語言學院")
 
 if st.button("進入英文學院"):
-    st.switch_page("pages/english_class.py")
+    st.switch_page("pages/4_english_class_英文學院.py")
 
 if st.button("進入中文學院"):
-    st.switch_page("pages/chinese_class.py")
+    st.switch_page("pages/3_chinese_class_中文學院.py")
 
 st.write("---")
 st.subheader("🍲 食品學院")
 
 if st.button("進入烘焙教室"):
-    st.switch_page("pages/baking_class.py")
+    st.switch_page("pages/2_baking_class_烘焙學院.py")
